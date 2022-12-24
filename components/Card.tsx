eb77fdbcf1c59e1413ad06e663e2061d51b32a20
distordev <adanuriplata@gmail.com>
2022-12-24 16:51:32 -0600

    Feat Added basic form

---
+++ components/Card.tsx
@@ -0,0 +1,51 @@
+import React from 'react';
+import { creditCard } from './FormCard';
+import { Inconsolata } from '@next/font/google';
+
+interface CardProps {
+  card: creditCard;
+}
+
+const inconsolata = Inconsolata({ subsets: ['latin'] });
+
+const Card = ({ card }: CardProps): JSX.Element => {
+  return (
+    <div className={inconsolata.className}>
+      <div className="py-4 px-3 w-72 bg-red-400 h-40 rounded-lg bg-gradient-to-r from-purple-600 to-blue-600">
+        <div className="pb-3 pt-2 flex justify-between">
+          <div>VISA</div>
+          <div>{`>>)`}</div>
+        </div>
+        <div className="text-2xl">
+          <p>{card.numCard}</p>
+        </div>
+        <div className="flex justify-between my-4 ">
+          <div>
+            <p className=" text-gray-300 text-xs"> Name</p>
+            <p className="uppercase text-xs">{card.name}</p>
+          </div>
+          <div className="text-center">
+            <p className=" text-gray-300 text-xs"> Expires</p>
+            <p className="uppercase text-xs">{card.expiration}</p>
+          </div>
+        </div>
+      </div>
+      <br />
+      <div className=" py-4 w-72 bg-red-400 h-40 rounded bg-gradient-to-r from-purple-600 to-blue-600">
+        <div className=" my-2 h-8 bg-gray-400 "></div>
+        <div>
+          <div className="flex justify-end px-3">
+            <div className=" bg-slate-100 text-gray-500 text-xs w-16 p-1 my-2 ">
+              CCV {card.ccv}
+            </div>
+          </div>
+          <div className="flex">
+            <div className=" w-4 h-8 bg-sky-200"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Card;
--- components/FormCard.tsx
+++ components/FormCard.tsx
@@ -1,19 +1,68 @@
+import { ChangeEvent, useState } from 'react';
+import Card from './Card';
+
+export interface creditCard {
+  numCard: string;
+  name: string;
+  ccv: string;
+  expiration: string;
+}
+
+const initialStateCard: creditCard = {
+  numCard: '0000000000000000',
+  name: 'fulanito de tal',
+  ccv: '000',
+  expiration: '00/00',
+};
+
 export const FormCard = (): JSX.Element => {
-  const { cardInfo, setCardInfo } = useState();
+  const [card, setCard] = useState<creditCard>(initialStateCard);
+
+  const handleChangeName = (e: ChangeEvent<HTMLInputElement>): void => {
+    setCard({ ...card, name: e.target.value });
+  };
+
+  const handleChangeCcv = (e: ChangeEvent<HTMLInputElement>): void => {
+    setCard({ ...card, ccv: e.target.value });
+  };
+
+  const handleChangeExpiration = (e: ChangeEvent<HTMLInputElement>): void => {
+    setCard({ ...card, expiration: e.target.value });
+  };
+
+  const hanfleChangeNumCard = (e: ChangeEvent<HTMLInputElement>): void => {
+    setCard({ ...card, numCard: e.target.value });
+  };
+
   return (
     <div>
-      <div>
-        <div className="flex">
-          <div>0000</div>
-          <div>0000</div>
-          <div>0000</div>
-          <div>0000</div>
+      <Card card={card} />
+      <div className=" mt-10 ">
+        <div>
+          <label htmlFor="">
+            Number card:
+            <input onChange={(e) => hanfleChangeNumCard(e)} type="text" />
+          </label>
         </div>
-        <div>12/22</div>
-        <div>NOMBRE</div>
-        <div>CCV</div>
+        <div>
+          <label htmlFor="">
+            Name:
+            <input onChange={(e) => handleChangeName(e)} type="text" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="">
+            Expiration
+            <input onChange={(e) => handleChangeExpiration(e)} type="text" />
+          </label>
+        </div>
+        <div>
+          <label htmlFor="">
+            CCV
+            <input onChange={(e) => handleChangeCcv(e)} type="text" />
+          </label>
+        </div>
       </div>
-      <div>Form</div>
     </div>
   );
 };
